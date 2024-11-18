b887a9c7cee8711168fdca81c7c9724337c7b90b
Romain Michau <romain9692@gmail.com>
2024-11-18 18:23:29 +0100

    fix

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -24,14 +24,20 @@ sudo apt update -y
 install_with_apt git
 
 # Clone .conf files
-alias rgit='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-git clone --bare git@github.com:RomainMichau/linux_config.git $HOME/.cfg
-rgit checkout
+if [ ! -d "$HOME/.rcfg" ]; then
+    alias rgit='/usr/bin/git --git-dir=$HOME/.rcfg/ --work-tree=$HOME'
+    rgit config --local status.showUntrackedFiles 
+    git clone --bare git@github.com:RomainMichau/linux_config.git $HOME/.rcfg
+    rm -f ~/.bashrc
+    rm -f ~/.zshrc
+    rgit checkout
+fi
 # END
 
 
 
 # ZSH
+install_with_apt fzf
 install_with_apt zsh
 chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -46,6 +52,9 @@ install_with_apt wget
 install_with_apt curl
 install_with_apt gpg
 install_with_apt vim
+
+sudo add-apt-repository -y ppa:maveonair/helix-editor
+install_with_apt helix
 
 # TOIL FOR VS CODE
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
